f073153317b199788589f746d7acd77cd322bf49
SpitfireGG <raambasnet02@gmail.com>
2024-05-01 12:45:56 +0545

    lsp & diagnostics

---
+++ config/plug/lsp/lsp.nix
@@ -0,0 +1,108 @@
+{
+  plugins = {
+    lsp-format = {enable = true;};
+    lsp = {
+      enable = true;
+      servers = {
+        eslint = {enable = true;};
+        html = {enable = true;};
+        lua-ls = {enable = true;};
+        nil_ls = {enable = true;};
+        marksman = {enable = false;};
+        pyright = {enable = false;};
+        gopls = {enable = true;};
+        terraformls = {enable = false;};
+        tsserver = {enable = false;};
+        rust-analyzer = {
+          enable = true;
+          installRustc = true;
+          installCargo = true;
+        };
+        htmx = {enable = true;};
+        clangd = {
+          enable = true;
+        };
+        bashls = {enable = true;};
+        zls = {enable = false;};
+        hls = {enable = false;};
+        yamlls = {
+          enable = true;
+        };
+      };
+      keymaps = {
+        silent = true;
+        lspBuf = {
+          gd = {
+            action = "definition";
+            desc = "Goto Definition";
+          };
+          gr = {
+            action = "references";
+            desc = "Goto References";
+          };
+          gD = {
+            action = "declaration";
+            desc = "Goto Declaration";
+          };
+          gI = {
+            action = "implementation";
+            desc = "Goto Implementation";
+          };
+          gT = {
+            action = "type_definition";
+            desc = "Type Definition";
+          };
+          K = {
+            action = "hover";
+            desc = "Hover";
+          };
+          "<leader>cw" = {
+            action = "workspace_symbol";
+            desc = "Workspace Symbol";
+          };
+          "<leader>cr" = {
+            action = "rename";
+            desc = "Rename";
+          };
+        };
+        diagnostic = {
+          "<leader>cd" = {
+            action = "open_float";
+            desc = "Line Diagnostics";
+          };
+          "[d" = {
+            action = "goto_next";
+            desc = "Next Diagnostic";
+          };
+          "]d" = {
+            action = "goto_prev";
+            desc = "Previous Diagnostic";
+          };
+        };
+      };
+    };
+  };
+  extraConfigLua = ''
+    local _border = "rounded"
+
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+      vim.lsp.handlers.hover, {
+        border = _border
+      }
+    )
+
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+      vim.lsp.handlers.signature_help, {
+        border = _border
+      }
+    )
+
+    vim.diagnostic.config{
+      float={border=_border}
+    };
+
+    require('lspconfig.ui.windows').default_options = {
+      border = _border
+    }
+  '';
+}
