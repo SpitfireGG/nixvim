441ee1a56b5d841497144cc85dd0978c94dc54eb
SpitfireGG <raambasnet02@gmail.com>
2024-05-06 23:06:37 +0545

    plugin[track::] +c

--- config/plug/lsp/per/clang.nix
+++ config/plug/lsp/per/clang.nix
@@ -6,16 +6,14 @@
       highlight = "Comment";
       showParameterHints = true;
 
-      parameter_hints_prefix = "<- ";
-      other_hints_prefix = "=> ";
-      max_len_align = false;
-      max_len_align_padding = 1;
+      parameterHintsPrefix = "<- ";
+      otherHintsPrefix = "=> ";
+      maxLenAlign = false;
+      maxLenAlignPadding = 1;
 
-      right_align = false;
-      right_align_padding = 7;
-
-      memoryUsage.border = null;
-      symbol_info = null;
+      rightAlign = false;
+      rightAlignPadding = 7;
     };
+    memoryUsage.border = null;
   };
 }
