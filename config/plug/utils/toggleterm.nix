4fa002dc56d343e0070598aaf380a74fd0b2c2f8
SpitfireGG <raambasnet02@gmail.com>
2024-05-12 20:04:19 +0545

    "horizontal term mapped to alt 1"

--- config/plug/utils/toggleterm.nix
+++ config/plug/utils/toggleterm.nix
@@ -1,16 +1,17 @@
 {
   plugins.toggleterm = {
     enable  = true;
-    extraOptions = {
+    settings = {
       auto_scroll = true; 
       autochdir = true;
       close_on_exit = true;
+      shade_terminals = false;
       direction = "horizontal";   
       hide_numbers = true;
       highlights =       { NormalFloat.link = "Normal"; FloatBorder.link = "Normal"; StatusLine.gui = "NONE"; StatusLineNC = { cterm = "italic"; gui = "NONE"; }; };
-      open_mapping = "[[m-1]]";
+      open_mapping = "[[<m-1>]]";
       persist_mode = true;
-      size = 15;
+      size = 10;
       float_opts.border = "single";
 
     };
