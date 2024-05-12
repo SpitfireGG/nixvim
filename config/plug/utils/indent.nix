b409cf979a4c2e8550bf847bdf8964557da64f39
SpitfireGG <raambasnet02@gmail.com>
2024-05-12 19:33:36 +0545

    --no-edit

---
+++ config/plug/utils/indent.nix
@@ -0,0 +1,32 @@
+{
+  plugins.indent-blankline = {
+    enable = true;
+    settings = {
+      indent.smart_indent_cap = true;
+      exclude = {
+        buftypes = [
+          "terminal"
+          "quickfix"
+        ];
+        filetypes = [
+          ""
+          "checkhealth"
+          "help"
+          "lspinfo"
+          "packer"
+          "TelescopePrompt"
+          "TelescopeResults"
+          "yaml"
+        ];
+      };
+      indent = {
+        char = "│";
+      };
+      scope = {
+        show_end = false;
+        show_exact_scope = true;
+        show_start = true;
+      };
+    };
+  };
+}
