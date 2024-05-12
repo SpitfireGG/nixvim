df6fb3c4d5230d7b80dc2aadd61a8bf3c0db8445
SpitfireGG <raambasnet02@gmail.com>
2024-05-12 23:11:32 +0545

    #️⃣ adde  ️⃣nvim-colorize

---
+++ config/plug/utils/colorizer.nix
@@ -0,0 +1,20 @@
+{
+  plugins.nvim-colorizer = {
+    enable =  true;
+    	bufTypes = {};
+      	user_default_options = {
+		RGB = true; # #RGB hex codes
+		RRGGBB = true; # #RRGGBB hex codes
+		names = false; # "Name" codes like Blue or blue
+		RRGGBBAA = true; # #RRGGBBAA hex codes
+		rgb_fn = true; # CSS rgb() and rgba() functions
+		hsl_fn = true; # CSS hsl() and hsla() functions
+		css = true; # Enable all CSS features: rgb_fn; hsl_fn; names; RGB; RRGGBB
+		css_fn = false; # Enable all CSS *functions*: rgb_fn; hsl_fn
+		# Available modes for `mode`: foreground; background;  virtualtext
+		mode = "background"; # Set the display mode.
+		tailwind = true; # Enable tailwind colors
+		virtualtext = "■";
+	};
+    };
+}
