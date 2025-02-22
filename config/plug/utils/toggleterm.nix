d189ad725908c1c3902eede41d9b82a290f7b867
SpitfireGG <raambasnet02@gmail.com>
2025-02-22 23:34:43 +0545

    now terminal closes with <C-q>

--- config/plug/utils/toggleterm.nix
+++ config/plug/utils/toggleterm.nix
@@ -1,19 +1,74 @@
 {
   plugins.toggleterm = {
-    enable  = true;
+    enable = true;
     settings = {
-      auto_scroll = true; 
+      auto_scroll = true;
       autochdir = true;
-      close_on_exit = true;
-      shade_terminals = false;
-      direction = "horizontal";   
-      hide_numbers = true;
-      highlights =       { NormalFloat.link = "Normal"; FloatBorder.link = "Normal"; StatusLine.gui = "NONE"; StatusLineNC = { cterm = "italic"; gui = "NONE"; }; };
-      open_mapping = "[[<m-1>]]";
-      persist_mode = true;
-      size = 10;
-      float_opts.border = "single";
+      close_on_exit = false; # Changed to false to debug shell exit issues
+      shade_terminals = true;
+      shading_factor = 2;
 
+      direction = "float";
+      hide_numbers = true;
+      highlights = {
+        NormalFloat.link = "Normal";
+        FloatBorder.link = "Normal";
+        StatusLine.gui = "NONE";
+        StatusLineNC = {
+          cterm = "italic";
+          gui = "NONE";
+        };
+      };
+      open_mapping = "[[<c-t>]]"; # Changed from <c-\> to avoid conflict
+      persist_mode = true;
+      persist_size = true;
+      insert_mappings = false; # Disable to prevent toggle loop
+      terminal_mappings = false; # Disable to prevent toggle loop
+
+      shell = "bash"; # Corrected to use Neovim’s shell
+      size = 10;
+      float_opts = {
+        border = "curved";
+        winblend = 0;
+      };
     };
   };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>tt";
+      action = "<cmd>ToggleTerm<cr>";
+      options = {
+        desc = "Toggle Terminal Window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<m-3>";
+      action = "<cmd>ToggleTerm direction=vertical<cr>";
+      options = {
+        desc = "Toggle Vertical Terminal Window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<m-2>";
+      action = "<cmd>ToggleTerm direction=horizontal<cr>";
+      options = {
+        desc = "Toggle Horizontal Terminal Window";
+      };
+    }
+    {
+      mode = "n";
+      key = "<m-1>";
+      action = "<cmd>ToggleTerm direction=float<cr>";
+      options = {
+        desc = "Toggle Floating Terminal Window";
+      };
+    }
+  ];
+  extraConfigLua = ''
+    -- Only enable <C-q> for closing, not opening
+    vim.keymap.set('t', '<C-q>', '<C-\\><C-n>:ToggleTerm<CR>', { noremap = true, silent = true })
+  '';
 }
