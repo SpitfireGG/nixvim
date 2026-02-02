977e12f7459196f6f937de72db4ab399cc98a0ed
archbishop <raambasnet02@gmail.com>
2026-02-03 00:32:33 +0545

    Remove inlay hints settings from clang.nix
    Removed inlay hints configuration from clang.nix.

--- config/core/lsp/specific/clang.nix
+++ config/core/lsp/specific/clang.nix
@@ -14,18 +14,7 @@
         };
       };
 
-      inlay_hints = {
-        highlight = "Comment";
-        show_parameter_hints = true;
-
-        parameter_hints_prefix = "<- ";
-        other_hints_prefix = "=> ";
-        max_len_align = false;
-        max_len_padding = 1;
-
-        right_align = false;
-        right_align_padding = 7;
-      };
+     
       memory_usage.border = null;
     };
   };
