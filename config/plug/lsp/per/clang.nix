36f60cf076312b25a531e820cadf6e7653ab0346
archbishop <raambasnet02@gmail.com>
2026-02-03 00:19:59 +0545

    Update clang.nix

--- config/plug/lsp/per/clang.nix
+++ config/plug/lsp/per/clang.nix
@@ -2,18 +2,6 @@
   plugins.clangd-extensions = {
     enable = true;
     enableOffsetEncodingWorkaround = true;
-    inlayHints = {
-      highlight = "Comment";
-      showParameterHints = true;
-
-      parameterHintsPrefix = "<- ";
-      otherHintsPrefix = "=> ";
-      maxLenAlign = false;
-      maxLenAlignPadding = 1;
-
-      rightAlign = false;
-      rightAlignPadding = 7;
-    };
     memoryUsage.border = null;
   };
 }
